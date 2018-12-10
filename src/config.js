627d613269d1da94e4d5b3e8f24995bf550b4d86
Michael Herwig <michael.herwig@rwth-aachen.de>
2018-12-10 02:16:48 +0100

    adjusted coin units

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 var api = 'http://207.180.240.152:10080';
 var apiList = [];
 var blockTargetInterval = 60;
-var coinUnits = 10000;
+var coinUnits = 1000000;
 var symbol = 'XIG';
 var refreshDelay = 30000;
