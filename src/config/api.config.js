6047cd020dd86f8037a207d2099e434ba31adff6
JerMe404 <3212692-jerme404@users.noreply.gitlab.com>
2019-02-19 12:06:34 -0500

    Moved api from forkmaps to xiproject

--- src/config/api.config.js
+++ src/config/api.config.js
@@ -1,9 +1,10 @@
 const Api = {
     //daemonAddress: 'http://192.168.1.220:22869',
-    daemonAddress: 'https://us-central1-forkmaps-214123.cloudfunctions.net',
+    //daemonAddress: 'https://us-central1-forkmaps-214123.cloudfunctions.net',
+    daemonAddress: 'https://us-central1-xiproject.cloudfunctions.net',
     apiList: [],
-    apiGetInfo: '/getinfo',
-    apiJsonRpc: '/json_rpc',
+    apiGetInfo: '/rpcGetInfo',
+    apiJsonRpc: '/rpcJson',
     jsonRpcBase: {
         jsonrpc: '2.0',
         //password: "xi123", // Optional
